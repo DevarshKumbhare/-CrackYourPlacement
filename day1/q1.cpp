55b8c5c40765d2a80c88a4697f071a252ebed2b2
Devarsh Kumbhare <96125830+DevarshKumbhare@users.noreply.github.com>
2024-07-14 18:38:30 +0530

    Master (#1)
    * day1 commit1
    
    * day1 commit2

---
+++ day1/q1.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>//Codeforces Round 877 (Div. 2)
+#include<algorithm>
+#include<array>
+#include<set>
+#include<bitset>
+#include<list>
+#include<iterator>
+#include<regex>
+#define ll long long
+#define ld long double
+#define endl '\n'
+#define ull unsigned long long
+#define ui unsigned int
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+const int N=10000;
+ 
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+};
